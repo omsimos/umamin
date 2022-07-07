741309ca5887d8a887d936551cc984f2bcaa2f7c
Josh Daniel <joshxfi.dev@gmail.com>
2022-07-07 12:08:28 +0800

    feat: setup assets

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}', './src/**/*.{js,ts,jsx,tsx}'],
+  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          100: "#F18FDF",
+          200: "#DA49BF",
+        },
+        secondary: {
+          100: "#27272A",
+          200: "#18181B",
+        },
+      },
+    },
   },
   plugins: [],
-}
+};
