ba22f4c1b774b8e84837e0c946729ffb6c881dfe
Josh Daniel <joshxfi.dev@gmail.com>
2022-07-14 20:55:06 +0800

    feat: add nprogress

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import '../styles/globals.css';
+import 'nprogress/nprogress.css';
+import Router from 'next/router';
+import NProgress from 'nprogress';
 import type { AppProps } from 'next/app';
 import { Toaster } from 'react-hot-toast';
 import { QueryClientProvider, Hydrate } from 'react-query';
 
 import { queryClient } from '@/api';
 import { Layout } from '@/components';
+
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
