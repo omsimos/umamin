807d621985050e01e1533adb1c288ff5cbfef679
アイザック <isaacdarcilla@gmail.com>
2022-07-29 10:11:49 +0800

    fix: swipe delete on messages (#51)

--- src/pages/inbox.tsx
+++ src/pages/inbox.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { useQuery, useMutation, dehydrate } from 'react-query';
 import { getSession, useSession } from 'next-auth/react';
 import { IoReload } from 'react-icons/io5';
@@ -23,6 +23,7 @@ const Inbox = () => {
   const [msgModal, setMsgModal] = useState(false);
   const [settingsModal, setSettingsModal] = useState(false);
   const [messageData, setMessageData] = useState({} as Partial<Message>);
+  const state = useRef({ x: 0 });
 
   const { data, status } = useSession();
   const { id, username } = data?.user ?? {};
@@ -42,25 +43,37 @@ const Inbox = () => {
 
   const { mutate } = useMutation(editMessage);
 
-  const handleOpen = (data: Partial<Message>) => {
-    setMessageData(data);
-
-    if (data.id && !data.isOpened) {
-      mutate(
-        {
-          id: data.id,
-          isOpened: true,
-        },
-        {
-          onSuccess: () => {
-            refetch();
-          },
-        }
-      );
-    }
-    setMsgModal(true);
-    triggerEvent('open_message');
+  const handleMouseDown = (e: { screenX: number }) => {
+    state.current.x = e.screenX;
   };
+
+  const handleOpen =
+    (data: Partial<Message>) =>
+    (e: { screenX: number; stopPropagation: () => void }) => {
+      setMessageData(data);
+
+      const delta = Math.abs(e.screenX - state.current.x);
+
+      if (delta < 10) {
+        e.stopPropagation();
+
+        if (data.id && !data.isOpened) {
+          mutate(
+            {
+              id: data.id,
+              isOpened: true,
+            },
+            {
+              onSuccess: () => {
+                refetch();
+              },
+            }
+          );
+        }
+        setMsgModal(true);
+        triggerEvent('open_message');
+      }
+    };
 
   const handleDelete = useMutation(deleteMessage);
 
@@ -114,33 +127,33 @@ const Inbox = () => {
       <div className='my-10 w-full text-left'>
         {messages?.length ? (
           <div className='mb-5 flex flex-col'>
-              <div className='flex justify-between'>
-                <p className='font-medium'>Latest messages</p>
-                <button type='button' onClick={() => refetch()}>
-                  <IoReload
-                    className={`text-lg ${
-                      isLoading || isRefetching ? 'animate-spin' : ''
-                    }`}
-                  />
-                </button>
-              </div>
-              <Info
-                className='text-xs'
-                message='Tap a card to reveal an anonymous message or swipe left to delete.'
-              />
-            </div>
-        ) : (
-          <div className='mb-5 text-center'>
-              <BiMessageDots className='mx-auto text-3xl' />
-              <p className='font-medium'>No messages yet.</p>
-              <button
-                type='button'
-                onClick={() => refetch()}
-                className='text-xs text-primary-100'
-              >
-                Tap to refresh
+            <div className='flex justify-between'>
+              <p className='font-medium'>Latest messages</p>
+              <button type='button' onClick={() => refetch()}>
+                <IoReload
+                  className={`text-lg ${
+                    isLoading || isRefetching ? 'animate-spin' : ''
+                  }`}
+                />
               </button>
             </div>
+            <Info
+              className='text-xs'
+              message='Tap a card to reveal an anonymous message or swipe left to delete.'
+            />
+          </div>
+        ) : (
+          <div className='mb-5 text-center'>
+            <BiMessageDots className='mx-auto text-3xl' />
+            <p className='font-medium'>No messages yet.</p>
+            <button
+              type='button'
+              onClick={() => refetch()}
+              className='text-xs text-primary-100'
+            >
+              Tap to refresh
+            </button>
+          </div>
         )}
 
         <div className='space-y-6'>
@@ -148,9 +161,15 @@ const Inbox = () => {
             <SwipeToDelete
               key={m.id}
               onDelete={() => {
-                handleDelete.mutate({ id: m.id });
-                toast.success('Message deleted');
-                refetch();
+                handleDelete.mutate(
+                  { id: m.id },
+                  {
+                    onSuccess: () => {
+                      toast.success('Message deleted');
+                      refetch();
+                    },
+                  }
+                );
               }}
               height={180}
               deleteWidth={100}
@@ -160,7 +179,8 @@ const Inbox = () => {
               <button
                 type='button'
                 key={m.id}
-                onClick={() => handleOpen(m)}
+                onMouseDown={handleMouseDown}
+                onClick={handleOpen(m)}
                 className='msg-card hide-tap-highlight h-[180px] w-full cursor-pointer overflow-hidden text-left'
               >
                 <div className='relative mb-3 h-[40px]'>
