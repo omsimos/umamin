cdad0f94e5aa9ecd5069c1ab3b6a96bcd4a94a42
Josh Daniel <joshxfi.dev@gmail.com>
2022-08-02 18:04:18 +0800

    chore: add ad units to resources

--- apps/web/src/pages/privacy-policy.tsx
+++ apps/web/src/pages/privacy-policy.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import dynamic from 'next/dynamic';
 import { markdown } from '@/constants';
 import { Markdown } from '@/components';
 
+const AdContainer = dynamic(() => import('@/components/AdContainer'), {
+  ssr: false,
+});
+
 const PrivacyPolicy = () => {
-  return <Markdown content={markdown.privacy} />;
+  return (
+    <section className='space-y-12'>
+      <AdContainer slot='7063833038' />
+      <Markdown content={markdown.privacy} />
+    </section>
+  );
 };
 
 export default PrivacyPolicy;
