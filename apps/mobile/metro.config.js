4c880fabadedf1857477553cea95cda96a2aae74
princejoogie <princejoogie@gmail.com>
2022-08-19 04:00:58 +0800

    add expo app

---
+++ apps/mobile/metro.config.js
@@ -0,0 +1,15 @@
+const { getDefaultConfig } = require('@expo/metro-config');
+const path = require('path');
+
+const workspaceRoot = path.resolve(__dirname, '../..');
+const projectRoot = __dirname;
+const config = getDefaultConfig(projectRoot);
+
+config.watchFolders = [workspaceRoot];
+
+config.resolver.nodeModulesPaths = [
+  path.resolve(projectRoot, 'node_modules'),
+  path.resolve(workspaceRoot, 'node_modules'),
+];
+
+module.exports = config;
