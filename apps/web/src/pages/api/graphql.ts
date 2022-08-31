7c2404640f5161abf6293f9200aeca4191a36dc2
Josh Daniel <joshxfi.dev@gmail.com>
2022-08-31 12:09:11 +0800

    chore: protect api with getToken

--- apps/web/src/pages/api/graphql.ts
+++ apps/web/src/pages/api/graphql.ts
@@ -1,8 +1,9 @@
 import 'reflect-metadata';
+import { NextApiRequest, NextApiResponse } from 'next';
 import { ApolloServer } from 'apollo-server-micro';
 import { buildSchema } from 'type-graphql';
 import { PrismaClient } from '@umamin/db';
-import Cors from 'micro-cors';
+import { getToken } from 'next-auth/jwt';
 
 import { UserResolver } from '@/schema/user';
 import { MessageResolver } from '@/schema/message';
@@ -12,11 +13,6 @@ const prisma = new PrismaClient();
 export interface TContext {
   prisma: typeof prisma;
 }
-
-const cors = Cors({
-  origin:
-    process.env.NODE_ENV === 'production' ? process.env.NEXTAUTH_URL : '*',
-});
 
 const schema = await buildSchema({
   resolvers: [UserResolver, MessageResolver],
@@ -36,14 +32,21 @@ export const config = {
 
 const startServer = server.start();
 
-export default cors(async (req, res) => {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   res.setHeader('Cache-Control', ['s-maxage=1', 'stale-while-revalidate']);
 
-  if (req.method === 'OPTIONS') {
-    res.end();
-    return false;
+  if (req.method !== 'POST') {
+    return res.status(405).json({ error: 'Method not allowed' });
+  }
+
+  const token = await getToken({ req });
+  if (!token) {
+    return res.status(401).json({ error: 'Not authorized' });
   }
 
   await startServer;
   return server.createHandler({ path: '/api/graphql' })(req, res);
-});
+}
