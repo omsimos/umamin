967652b4f8c803d5a8f463bc621d262016e3d16f
Josh Daniel <joshxfi.dev@gmail.com>
2023-02-25 15:47:19 +0800

    chore: exclude reply in seen resolver

--- packages/generated/index.ts
+++ packages/generated/index.ts
@@ -160,7 +160,7 @@ export type GetSeenMessagesQueryVariables = Exact<{
 }>;
 
 
-export type GetSeenMessagesQuery = { __typename?: 'Query', getSeenMessages?: Array<{ __typename?: 'SeenMessage', id: string, clue?: string | null, reply?: string | null, content: string, createdAt: any, receiverMsg: string }> | null };
+export type GetSeenMessagesQuery = { __typename?: 'Query', getSeenMessages?: Array<{ __typename?: 'SeenMessage', id: string, clue?: string | null, content: string, createdAt: any, receiverMsg: string }> | null };
 
 export type GetSentMessagesQueryVariables = Exact<{
   cursorId?: InputMaybe<Scalars['ID']>;
@@ -258,7 +258,6 @@ export const GetSeenMessagesDocument = gql`
   getSeenMessages(cursorId: $cursorId) {
     id
     clue
-    reply
     content
     createdAt
     receiverMsg
