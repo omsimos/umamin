82b5c687e664463579721197398982e88e867fd4
Josh Daniel <joshxfi.dev@gmail.com>
2023-08-14 22:01:44 +0800

    feat: add maintenance page for global

--- apps/web/src/pages/global.tsx
+++ apps/web/src/pages/global.tsx
@@ -55,89 +55,100 @@ const Global: NextPageWithLayout = () => {
       />
 
       <section className='mx-auto max-w-lg pb-52'>
-        <Container className='flex flex-col mb-12'>
-          <div className='bg-secondary-200 p-8 rounded-2xl'>
-            <div className='relative inline-block'>
-              <p className='mb-4 text-base text-secondary-400'>
-                Global Messages
-              </p>
-              <p className='absolute -top-2 -right-12 text-[10px] font-bold bg-primary-200 rounded-full px-2 tracking-wide'>
-                BETA
-              </p>
-            </div>
-
-            <div className='flex gap-x-2 items-center'>
-              <ImageFill
-                alt='profile picture'
-                src={data?.user?.image}
-                unoptimized
-                className='border-secondary-100 h-[45px] w-[45px] object-cover rounded-full border flex-none'
-              />
-
-              <button
-                type='button'
-                onClick={() => {
-                  if (!user) {
-                    push('/login');
-                  } else {
-                    setSendGlobalModal(true);
-                  }
-                }}
-                className='rounded-full w-full bg-secondary-100 px-6 py-3 text-secondary-400 text-left'
-              >
-                Send a global message &rarr;
-              </button>
-            </div>
-          </div>
-        </Container>
-
-        <AdContainer slotId='7492157497' className='mb-4' />
-
-        <div className='space-y-12 pt-12'>
-          {messageData && (
-            <GlobalPost message={messageData} key={messageData.id} />
-          )}
-          {messages?.map((m) => (
-            <GlobalPost message={m} key={m.id} />
-          ))}
-        </div>
-
-        <AdContainer slotId='2048259127' className='my-4' />
-
-        {!isLoading && messages && messages?.length > 0 && (
-          <Container
-            className={`flex mt-4 ${
-              cursorId ? 'justify-between' : 'justify-end'
-            }`}
-          >
-            {cursorId && (
-              <button
-                className='hover:underline'
-                onClick={() => {
-                  setPageNo(1);
-                  setCursorId('');
-                }}
-                type='button'
-              >
-                &larr; Latest
-              </button>
-            )}
-
-            {cursorId && <p>{pageNo}</p>}
-
-            {messages.length === 10 && (
-              <button
-                className='hover:underline'
-                onClick={() => {
-                  setPageNo(cursorId ? pageNo + 1 : 2);
-                  setCursorId(messages?.length ? messages[9]?.id! : '');
-                }}
-                type='button'
-              >
-                More &rarr;
-              </button>
-            )}
+        {process.env.NEXT_PUBLIC_GLOBAL_MAINTENANCE === 'true' ? (
+          <Container>
+            <h1 className='text-4xl font-semibold'>We&quot;ll be back!</h1>
+            <p className='text-secondary-400 mt-1'>
+              This page is currently taking a break
+            </p>
           </Container>
+        ) : (
+          <>
+            <Container className='flex flex-col mb-12'>
+              <div className='bg-secondary-200 p-8 rounded-2xl'>
+                <div className='relative inline-block'>
+                  <p className='mb-4 text-base text-secondary-400'>
+                    Global Messages
+                  </p>
+                  <p className='absolute -top-2 -right-12 text-[10px] font-bold bg-primary-200 rounded-full px-2 tracking-wide'>
+                    BETA
+                  </p>
+                </div>
+
+                <div className='flex gap-x-2 items-center'>
+                  <ImageFill
+                    alt='profile picture'
+                    src={data?.user?.image}
+                    unoptimized
+                    className='border-secondary-100 h-[45px] w-[45px] object-cover rounded-full border flex-none'
+                  />
+
+                  <button
+                    type='button'
+                    onClick={() => {
+                      if (!user) {
+                        push('/login');
+                      } else {
+                        setSendGlobalModal(true);
+                      }
+                    }}
+                    className='rounded-full w-full bg-secondary-100 px-6 py-3 text-secondary-400 text-left'
+                  >
+                    Send a global message &rarr;
+                  </button>
+                </div>
+              </div>
+            </Container>
+
+            <AdContainer slotId='7492157497' className='mb-4' />
+
+            <div className='space-y-12 pt-12'>
+              {messageData && (
+                <GlobalPost message={messageData} key={messageData.id} />
+              )}
+              {messages?.map((m) => (
+                <GlobalPost message={m} key={m.id} />
+              ))}
+            </div>
+
+            <AdContainer slotId='2048259127' className='my-4' />
+
+            {!isLoading && messages && messages?.length > 0 && (
+              <Container
+                className={`flex mt-4 ${
+                  cursorId ? 'justify-between' : 'justify-end'
+                }`}
+              >
+                {cursorId && (
+                  <button
+                    className='hover:underline'
+                    onClick={() => {
+                      setPageNo(1);
+                      setCursorId('');
+                    }}
+                    type='button'
+                  >
+                    &larr; Latest
+                  </button>
+                )}
+
+                {cursorId && <p>{pageNo}</p>}
+
+                {messages.length === 10 && (
+                  <button
+                    className='hover:underline'
+                    onClick={() => {
+                      setPageNo(cursorId ? pageNo + 1 : 2);
+                      setCursorId(messages?.length ? messages[9]?.id! : '');
+                    }}
+                    type='button'
+                  >
+                    More &rarr;
+                  </button>
+                )}
+              </Container>
+            )}
+          </>
         )}
 
         {user && <BottomNavbar />}
