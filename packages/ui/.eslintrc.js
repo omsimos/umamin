4ac329dd2efd97a45cde760d0e02448d8b138687
Josh Daniel <joshxfi.dev@gmail.com>
2024-03-17 21:18:43 +0800

    feat: add fresh install of nextjs

--- packages/ui/.eslintrc.js
+++ packages/ui/.eslintrc.js
@@ -1,10 +1,9 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
   root: true,
-  extends: ["@repo/eslint-config/react-internal.js"],
+  extends: ["@umamin/eslint-config/react-internal.js"],
   parser: "@typescript-eslint/parser",
-  parserOptions: {
-    project: "./tsconfig.lint.json",
-    tsconfigRootDir: __dirname,
+  rules: {
+    "no-redeclare": "off",
   },
 };
