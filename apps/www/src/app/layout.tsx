dde8e4b86603677c2fb7475fccaf02fa6eb34401
Josh Daniel <joshxfi.dev@gmail.com>
2024-03-18 15:50:02 +0800

    chore: use dark mode

--- apps/www/src/app/layout.tsx
+++ apps/www/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
+import "@umamin/ui/globals.css";
+import { cn } from "@ui/lib/utils";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body
+        className={cn(inter.className, "bg-background text-foreground dark")}
+      >
+        {children}
+      </body>
     </html>
   );
 }
