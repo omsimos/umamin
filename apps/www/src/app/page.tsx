87e14dff25c766ca631f885fb305f3d0aac96f56
hyamero <daleban.dev@gmail.com>
2024-03-19 00:27:21 +0800

    chore: inbox route with base ui

--- apps/www/src/app/page.tsx
+++ apps/www/src/app/page.tsx
@@ -1,10 +1,19 @@
 import { Button } from "@umamin/ui/components/button";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main>
-      <h1>Hello</h1>
-      <Button>World</Button>
+    <main className="h-screen grid place-items-center">
+      <div className="text-center">
+        <h1 className="font-bold text-6xl">hello, umamin</h1>
+        <p>💌 The ultimate platform for anonymous messages!</p>
+        <Link href="/to/user" className="mt-5 flex gap-2">
+          <Button className="w-full">Send Message</Button>
+          <Button className="w-full" variant="secondary">
+            View Profile
+          </Button>
+        </Link>
+      </div>
     </main>
   );
 }
---
+++ apps/www/src/app/to/[slug]/page.tsx
@@ -0,0 +1,9 @@
+import { ChatBox } from "@/app/components/chatbox";
+
+export default function Home() {
+  return (
+    <main className="h-screen grid place-items-center">
+      <ChatBox />
+    </main>
+  );
+}
