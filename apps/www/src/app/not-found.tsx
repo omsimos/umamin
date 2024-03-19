9da2c0dee189851c8131b03b75a5eb918f9255e0
hyamero <daleban.dev@gmail.com>
2024-03-19 11:46:00 +0800

    feat: 404 page

---
+++ apps/www/src/app/not-found.tsx
@@ -0,0 +1,18 @@
+import { Button } from "@umamin/ui/components/button";
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <main className="h-screen grid place-items-center">
+      <div className="text-center">
+        <h1 className="font-bold text-6xl">Are you lost?</h1>
+        <Link href="/" className="mt-5 flex gap-2">
+          <Button className="w-full">Back Home</Button>
+          <Button className="w-full" variant="secondary">
+            View Profile
+          </Button>
+        </Link>
+      </div>
+    </main>
+  );
+}
