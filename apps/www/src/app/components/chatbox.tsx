1b377a0496783798402377d753c46c8046a744a4
hyamero <daleban.dev@gmail.com>
2024-03-19 11:38:46 +0800

    feat: profile hover card on hover

--- apps/www/src/app/components/chatbox.tsx
+++ apps/www/src/app/components/chatbox.tsx
@@ -1,27 +1,25 @@
 "use client";
 
-import * as React from "react";
+import { useState } from "react";
 import { Send } from "lucide-react";
+import { ChatList } from "./chat-list";
 
 import { Input } from "@umamin/ui/components/input";
 import { Button } from "@umamin/ui/components/button";
-
+import { ProfileHoverCard } from "./profile-hover-card";
 import { Card, CardFooter, CardHeader } from "@umamin/ui/components/card";
-
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipProvider,
-  TooltipTrigger,
-} from "@umamin/ui/components/tooltip";
-
-import { ChatList } from "./chat-list";
-import { useState } from "react";
 
 export function ChatBox() {
   const [input, setInput] = useState("");
 
-  const messages = [
+  const _recipient = {
+    name: "John Doe",
+    slug: "johndoe",
+    id: "123",
+    createdAt: "2021-08-01",
+  };
+
+  const _messages = [
     {
       role: "recipient",
       content:
@@ -37,29 +35,22 @@ export function ChatBox() {
     return null;
   };
 
-  const inputLength = input.trim().length;
-
   return (
     <Card className="flex flex-col  justify-between w-full max-w-2xl h-[70vh] min-h-[350px] max-h-[450px] relative">
       <CardHeader className="bg-background border-b w-full item-center rounded-t-2xl flex justify-between flex-row">
         <div className="flex items-center space-x-2">
           <span className="text-muted-foreground">To:</span>
-          <TooltipProvider delayDuration={0}>
-            <Tooltip>
-              <TooltipTrigger asChild>
-                <div>
-                  <p className="font-medium leading-none">@johndoe</p>
-                </div>
-              </TooltipTrigger>
-              <TooltipContent className="max-w-xs" sideOffset={25}>
-                ⚡ We can add the user status here! 😲
-              </TooltipContent>
-            </Tooltip>
-          </TooltipProvider>
+          <ProfileHoverCard user={_recipient} sessionId="456">
+            <div>
+              <p className="font-medium leading-none cursor-pointer">
+                @johndoe
+              </p>
+            </div>
+          </ProfileHoverCard>
         </div>
       </CardHeader>
 
-      <ChatList messages={messages} />
+      <ChatList messages={_messages} />
 
       <CardFooter className="flex w-full flex-col z-10">
         <form
@@ -74,7 +65,11 @@ export function ChatBox() {
             value={input}
             onChange={(e) => setInput(e.target.value)}
           />
-          <Button type="submit" size="icon" disabled={inputLength === 0}>
+          <Button
+            type="submit"
+            size="icon"
+            disabled={input.trim().length === 0}
+          >
             <Send className="h-4 w-4" />
             <span className="sr-only">Send</span>
           </Button>
