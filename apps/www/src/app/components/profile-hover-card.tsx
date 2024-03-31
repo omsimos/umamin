988d175f554793e6d7e85564002d98edeb02057c
hyamero <daleban.dev@gmail.com>
2024-03-31 22:17:45 +0800

    chore: update pulse post ui

--- apps/www/src/app/components/profile-hover-card.tsx
+++ apps/www/src/app/components/profile-hover-card.tsx
@@ -43,30 +43,30 @@ export function ProfileHoverCard({
   return (
     <HoverCard openDelay={300}>
       <HoverCardTrigger asChild>{children}</HoverCardTrigger>
-      <HoverCardContent className="w-80">
-        <div className="flex items justify-between space-x-4">
-          <div className="space-y-1">
+      <HoverCardContent className='w-80'>
+        <div className='flex items justify-between space-x-4'>
+          <div className='space-y-1'>
             <div>
-              <h4 className="text-lg font-semibold">{user.name}</h4>
+              <h4 className='text-lg font-semibold'>{user.name}</h4>
               {user.slug && (
                 <Link
                   href={`/user/${userSlug}`}
-                  className="text-sm font-normal text-zinc-200"
+                  className='text-sm font-normal text-muted-foreground'
                 >
                   {"@" + user.slug}
                 </Link>
               )}
             </div>
-            <div className="flex items-center">
-              <CalendarDays className="mr-1 h-3 w-3 text-muted-foreground" />{" "}
-              <span className="text-xs text-muted-foreground">
+            <div className='flex items-center'>
+              <CalendarDays className='mr-1 h-3 w-3 text-muted-foreground' />{" "}
+              <span className='text-xs text-muted-foreground'>
                 {/* Joined {format(new Date(user.createdAt), "MMMM yyyy")} */}
                 Joined March 19, 2024
               </span>
             </div>
           </div>
 
-          <Link href={`/user/${userSlug}`} className="font-semibold">
+          <Link href={`/user/${userSlug}`} className='font-semibold'>
             {/* <Avatar className="h-16 w-16">
               <AvatarImage
                 className="rounded-full"
@@ -77,22 +77,22 @@ export function ProfileHoverCard({
                 {user.name?.split(" ").at(0)}
               </AvatarFallback>
             </Avatar> */}
-            <Avatar className="h-16 w-16">
+            <Avatar className='h-16 w-16'>
               <AvatarFallback>
                 <ScanFace />
               </AvatarFallback>
             </Avatar>
           </Link>
         </div>
-        <p className="text-muted-foreground text-sm mt-3">
+        <p className='text-muted-foreground text-sm mt-3'>
           The quick brown fox jumps over the lazy dog near the bank of the
           river🦊
         </p>
         {user.id !== sessionId && (
           <Button
-            title="follow"
+            title='follow'
             onClick={() => toast.info("Feature coming soon!")}
-            className="mt-4 w-full"
+            className='mt-4 w-full'
           >
             Follow
           </Button>
--- apps/www/src/app/pulse/components/pulse-item.tsx
+++ apps/www/src/app/pulse/components/pulse-item.tsx
@@ -13,77 +13,77 @@ import {
 } from "@umamin/ui/components/tooltip";
 import { PulseDropDownMenu } from "./dropdown-menu";
 import { SendMessageDrawer } from "./send-message-drawer";
+import { Card, CardContent, CardHeader } from "@umamin/ui/components/card";
 
 export default function PulseItem() {
   return (
-    <div className='flex items-start justify-between border-b py-10 text-[#f2f4f6] last:border-0'>
-      <div className='flex w-full items-start gap-3'>
-        <Link href={`/user/johndoe`} className='font-semibold'>
-          {/* <Avatar className="relative top-1">
+    <Card className='flex flex-col items-start justify-between'>
+      <CardHeader className='w-full'>
+        <div className='flex justify-between'>
+          <div className='flex gap-4'>
+            <Link href={`/user/johndoe`} className='font-semibold'>
+              {/* <Avatar className="relative top-1">
             <AvatarImage
-              className="rounded-full"
-              src={post.author.image as string | undefined}
-              alt={`${post.author.name}'s avatar`}
+            className="rounded-full"
+            src={post.author.image as string | undefined}
+            alt={`${post.author.name}'s avatar`}
             />
             <AvatarFallback className="text-xs">
-              {post.author.name?.split(" ").at(0)}
+            {post.author.name?.split(" ").at(0)}
             </AvatarFallback>
           </Avatar> */}
-          <Avatar>
-            <AvatarFallback>
-              <ScanFace />
-            </AvatarFallback>
-          </Avatar>
-        </Link>
-
-        <div className='w-full space-y-1'>
-          <div className='flex justify-between'>
-            <Link
-              href={`/user/$johndoe`}
-              className='font-semibold hover:underline'
-            >
-              {/* {post.author.name} */}
-              johndoe
+              <Avatar>
+                <AvatarFallback>
+                  <ScanFace />
+                </AvatarFallback>
+              </Avatar>
             </Link>
 
-            {/* 
-             DropDownMenu
-            */}
-
-            <div className='flex gap-1 text-muted-foreground items-center'>
-              <SendMessageDrawer />
-              <PulseDropDownMenu />
-            </div>
-          </div>
-
-          <TooltipProvider>
-            <Tooltip>
-              <TooltipTrigger className='text-sm text-muted-foreground'>
-                {/* <span className="select-none text-muted-foreground">
+            <div className='flex flex-col'>
+              <Link
+                href={`/user/$johndoe`}
+                className='font-semibold hover:underline'
+              >
+                {/* {post.author.name} */}
+                johndoe
+              </Link>
+              <TooltipProvider>
+                <Tooltip>
+                  <TooltipTrigger className='text-sm text-muted-foreground'>
+                    {/* <span className="select-none text-muted-foreground">
                       {formatDistanceToNowStrict(post.createdAt, {
                         addSuffix: false,
                         locale: {
                           formatDistance: (...props) =>
-                            formatDistance(...props),
+                          formatDistance(...props),
                         },
                       })}
                     </span> */}
-                4h · umamin.link
-              </TooltipTrigger>
-              <TooltipContent>
-                {/* <span>{formatRelative(post.createdAt, new Date())}</span> */}
-                full date
-              </TooltipContent>
-            </Tooltip>
-          </TooltipProvider>
+                    4h · umamin.link
+                  </TooltipTrigger>
+                  <TooltipContent>
+                    {/* <span>{formatRelative(post.createdAt, new Date())}</span> */}
+                    full date
+                  </TooltipContent>
+                </Tooltip>
+              </TooltipProvider>
+            </div>
+          </div>
 
-          <p className='whitespace-pre-wrap'>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti
-            omnis quam dicta provident reiciendis itaque eum veniam non dolorum
-            aperiam?
-          </p>
+          <div className='flex gap-1 text-muted-foreground items-center'>
+            <SendMessageDrawer />
+            <PulseDropDownMenu />
+          </div>
         </div>
-      </div>
-    </div>
+      </CardHeader>
+
+      <CardContent className='flex w-full gap-3'>
+        <p className='whitespace-pre-wrap'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti sd
+          omnis quam dicta provident reiciendis itaque eum veniam non dolorum
+          aperiam?
+        </p>
+      </CardContent>
+    </Card>
   );
 }
