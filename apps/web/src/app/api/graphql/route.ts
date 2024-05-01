3829718c4dc138d30cfb6af2e93c97d616f22689
Josh Daniel <joshxfi.dev@gmail.com>
2024-05-01 20:47:49 +0800

    chore: setup cors

--- apps/web/src/app/api/graphql/route.ts
+++ apps/web/src/app/api/graphql/route.ts
@@ -1,10 +1,18 @@
 import { gqlSchema } from "@umamin/server";
 import { createYoga } from "graphql-yoga";
 
+const origin = process.env.NEXT_PUBLIC_VERCEL_URL;
+
 const { handleRequest } = createYoga({
   schema: gqlSchema,
   graphqlEndpoint: "/api/graphql",
   fetchAPI: { Response },
+  cors: {
+    origin: origin ? `https://${origin}` : "http://localhost:3000",
+    credentials: true,
+    allowedHeaders: ["Content-Type, Authorization"],
+    methods: ["POST"],
+  },
 });
 
 export {
--- apps/web/src/app/user/[username]/page.tsx
+++ apps/web/src/app/user/[username]/page.tsx
@@ -33,7 +33,7 @@ export default async function Page({
   const user = result.data?.getUserByUsername;
 
   if (!user) {
-    redirect("/");
+    redirect("/404");
   }
 
   return (
