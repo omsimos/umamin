ce1019590458d2ee8a08ff77dec60c729a404eea
Josh Daniel <joshxfi.dev@gmail.com>
2024-05-01 21:06:06 +0800

    feat: implement csrf prevention and apq

--- apps/web/src/app/api/graphql/route.ts
+++ apps/web/src/app/api/graphql/route.ts
@@ -1,5 +1,7 @@
 import { gqlSchema } from "@umamin/server";
 import { createYoga } from "graphql-yoga";
+import { useAPQ } from "@graphql-yoga/plugin-apq";
+import { useCSRFPrevention } from "@graphql-yoga/plugin-csrf-prevention";
 
 const origin = process.env.NEXT_PUBLIC_VERCEL_URL;
 
@@ -13,6 +15,12 @@ const { handleRequest } = createYoga({
     allowedHeaders: ["Content-Type, Authorization"],
     methods: ["POST"],
   },
+  plugins: [
+    useCSRFPrevention({
+      requestHeaders: ["x-graphql-yoga-csrf"],
+    }),
+    useAPQ(),
+  ],
 });
 
 export {
