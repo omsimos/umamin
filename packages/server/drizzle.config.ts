746eed11fd031dc704efdbfccb9bbab45c59fc7d
Josh Daniel <joshxfi.dev@gmail.com>
2024-05-13 19:39:08 +0800

    fix: migrate drizzle config to 0.21

--- packages/server/drizzle.config.ts
+++ packages/server/drizzle.config.ts
@@ -1,7 +1,7 @@
-import "dotenv/config";
-import type { Config } from "drizzle-kit";
+import { defineConfig } from "drizzle-kit";
 
-export default {
+export default defineConfig({
+  dialect: "sqlite",
   schema: "./db/schema/*",
   out: "./db/migrations",
   driver: "turso",
@@ -9,4 +9,4 @@ export default {
     url: process.env.TURSO_CONNECTION_URL!,
     authToken: process.env.TURSO_AUTH_TOKEN!,
   },
-} satisfies Config;
+});
