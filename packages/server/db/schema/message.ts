d07ea0ef92623ab29a6b84d9c47714a08c085ece
Josh Daniel <joshxfi.dev@gmail.com>
2024-05-14 17:14:51 +0800

    fix: disambiguate messages relations

--- packages/server/db/schema/message.ts
+++ packages/server/db/schema/message.ts
@@ -47,10 +47,12 @@ export const messagesRelations = relations(message, ({ one }) => ({
   user: one(user, {
     fields: [message.userId],
     references: [user.id],
+    relationName: "message_receiver",
   }),
   sender: one(user, {
     fields: [message.senderId],
     references: [user.id],
+    relationName: "message_sender",
   }),
 }));
 
