161299c3dcdb6cf69685ea930cc65d209b1188bb
Josh Daniel <joshxfi.dev@gmail.com>
2024-05-26 19:28:30 +0800

    chore: update 404

--- apps/web/src/app/not-found.tsx
+++ apps/web/src/app/not-found.tsx
@@ -1,16 +1,12 @@
-import { Button } from "@umamin/ui/components/button";
 import Link from "next/link";
 
 export default function NotFound() {
   return (
     <main className="h-screen grid place-items-center">
       <div className="text-center">
-        <h1 className="font-bold text-6xl">Are you lost?</h1>
-        <Link href="/" className="mt-5 flex gap-2">
-          <Button className="w-full">Back Home</Button>
-          <Button className="w-full" variant="secondary">
-            View Profile
-          </Button>
+        <h1 className="font-bold md:text-6xl text-4xl mb-4">Oops, Not Found</h1>
+        <Link href="/" className="text-muted-foreground underline md:text-base text-sm">
+          Go Back &rarr;
         </Link>
       </div>
     </main>
