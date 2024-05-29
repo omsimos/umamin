6cff9b56338f8073571580db7625aa627c695adf
Josh Daniel <joshxfi.dev@gmail.com>
2024-05-29 20:38:42 +0800

    chore: re-initialize migration

--- packages/server/db/migrations/0000_sad_triton.sql
+++ packages/server/db/migrations/0000_sad_triton.sql
@@ -18,7 +18,6 @@ CREATE TABLE `session` (
 CREATE TABLE `user` (
 	`id` text PRIMARY KEY NOT NULL,
 	`username` text NOT NULL,
-	`note` text,
 	`bio` text,
 	`image_url` text,
 	`quiet_mode` integer DEFAULT false NOT NULL,
@@ -27,21 +26,30 @@ CREATE TABLE `user` (
 	`updated_at` integer
 );
 --> statement-breakpoint
+CREATE TABLE `note` (
+	`id` text PRIMARY KEY NOT NULL,
+	`user_id` text NOT NULL,
+	`content` text NOT NULL,
+	`is_anonymous` integer NOT NULL,
+	`created_at` integer DEFAULT (unixepoch()) NOT NULL,
+	`updated_at` integer,
+	FOREIGN KEY (`user_id`) REFERENCES `user`(`id`) ON UPDATE no action ON DELETE cascade
+);
+--> statement-breakpoint
 CREATE TABLE `message` (
 	`id` text PRIMARY KEY NOT NULL,
 	`question` text NOT NULL,
 	`content` text NOT NULL,
-	`user_id` text NOT NULL,
+	`receiver_id` text NOT NULL,
 	`sender_id` text,
 	`created_at` integer DEFAULT (unixepoch()) NOT NULL,
-	FOREIGN KEY (`user_id`) REFERENCES `user`(`id`) ON UPDATE no action ON DELETE cascade,
+	FOREIGN KEY (`receiver_id`) REFERENCES `user`(`id`) ON UPDATE no action ON DELETE cascade,
 	FOREIGN KEY (`sender_id`) REFERENCES `user`(`id`) ON UPDATE no action ON DELETE cascade
 );
 --> statement-breakpoint
 CREATE UNIQUE INDEX `user_username_unique` ON `user` (`username`);--> statement-breakpoint
-CREATE INDEX `note_updated_at_idx` ON `user` (`note`,`updated_at`);--> statement-breakpoint
-CREATE INDEX `note_updated_at_id_idx` ON `user` (`note`,`updated_at`,`id`);--> statement-breakpoint
-CREATE INDEX `user_id_created_at_idx` ON `message` (`user_id`,`created_at`);--> statement-breakpoint
-CREATE INDEX `user_id_created_at_id_idx` ON `message` (`user_id`,`created_at`,`id`);--> statement-breakpoint
-CREATE INDEX `sender_id_created_at_idx` ON `message` (`sender_id`,`created_at`);--> statement-breakpoint
-CREATE INDEX `sender_id_created_at_id_idx` ON `message` (`user_id`,`created_at`,`id`);
+CREATE UNIQUE INDEX `note_user_id_unique` ON `note` (`user_id`);--> statement-breakpoint
+CREATE INDEX `id_updated_at_idx` ON `note` (`id`,`updated_at`);--> statement-breakpoint
+CREATE INDEX `receiver_id_idx` ON `message` (`receiver_id`);--> statement-breakpoint
+CREATE INDEX `sender_id_idx` ON `message` (`sender_id`);--> statement-breakpoint
+CREATE INDEX `created_at_id_idx` ON `message` (`created_at`,`id`);
