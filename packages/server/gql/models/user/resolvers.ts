7658b38573767ea1503179895de776092115e7f2
Josh Daniel <joshxfi.dev@gmail.com>
2024-06-02 15:18:19 +0800

    chore: add create user resolver

--- packages/server/gql/models/user/resolvers.ts
+++ packages/server/gql/models/user/resolvers.ts
@@ -1,4 +1,6 @@
+import { nanoid } from "nanoid";
 import { eq } from "drizzle-orm";
+import { hash } from "@node-rs/argon2";
 import { GraphQLError } from "graphql";
 
 import { db } from "../../../db";
@@ -32,6 +34,49 @@ builder.queryFields((t) => ({
 }));
 
 builder.mutationFields((t) => ({
+  createUser: t.field({
+    type: "String",
+    nullable: true,
+    args: {
+      username: t.arg.string({ required: true }),
+      password: t.arg.string({ required: true }),
+    },
+    resolve: async (_, args) => {
+      try {
+        if (!/^[a-z0-9_-]+$/.test(args.username)) {
+          throw new GraphQLError(
+            "Username must be alphanumeric with no spaces.",
+          );
+        }
+
+        const userId = nanoid();
+        const passwordHash = await hash(args.password, {
+          memoryCost: 19456,
+          timeCost: 2,
+          outputLen: 32,
+          parallelism: 1,
+        });
+
+        await db.insert(user).values({
+          id: userId,
+          username: args.username,
+          passwordHash,
+        });
+
+        return userId;
+      } catch (err: any) {
+        console.log(err);
+
+        if (err.code === "SQLITE_CONSTRAINT") {
+          if (err.message.includes("user.username")) {
+            throw new GraphQLError("Username is already taken.");
+          }
+        }
+        throw err;
+      }
+    },
+  }),
+
   updateUser: t.field({
     type: "User",
     authScopes: {
