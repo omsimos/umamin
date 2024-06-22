e327b393ffc6935b0a7931f9085d34c3e7bbf805
hyamero <daleban.dev@gmail.com>
2024-06-22 18:57:27 +0800

    chore: animated shiny button

--- packages/ui/tailwind.config.ts
+++ packages/ui/tailwind.config.ts
@@ -71,10 +71,19 @@ const config = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: "0" },
         },
+        shimmer: {
+          "0%, 90%, 100%": {
+            "background-position": "calc(-100% - var(--shimmer-width)) 0",
+          },
+          "30%, 60%": {
+            "background-position": "calc(100% + var(--shimmer-width)) 0",
+          },
+        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+        shimmer: "shimmer 8s infinite",
       },
     },
   },
