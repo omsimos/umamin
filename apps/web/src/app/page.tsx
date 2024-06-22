c42d5c2ec76a6889e84c06d94959d7ffe29c4119
hyamero <daleban.dev@gmail.com>
2024-06-22 19:43:40 +0800

    chore: subtle grid demo background

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import { ExternalLink, Lock } from "lucide-react";
+import { ExternalLink } from "lucide-react";
 
 import { Demo } from "./components/demo";
 import { Button } from "@umamin/ui/components/button";
@@ -8,7 +8,7 @@ import { cn } from "@umamin/ui/lib/utils";
 
 export default async function Home() {
   return (
-    <main className="flex items-center flex-col min-h-screen lg:pt-44 py-36 container">
+    <main className="flex items-center flex-col min-h-screen lg:pt-44 py-36 gap-8 container">
       <Link
         href="https://v1.umamin.link"
         target="_blank"
@@ -24,13 +24,13 @@ export default async function Home() {
         </AnimatedShinyText>
       </Link>
 
-      <div className="border-b-2 border-muted border-dashed py-8">
+      <div className="border-b-2 border-muted border-dashed pb-8">
         <h1 className="font-extrabold md:text-7xl text-[10vw] leading-none bg-gradient-to-b from-foreground to-zinc-400 bg-clip-text text-transparent tracking-tighter text-center">
           The Platform for Anonymity
         </h1>
       </div>
 
-      <p className="text-muted-foreground mt-8 text-center md:text-xl max-w-2xl">
+      <p className="text-muted-foreground text-center md:text-xl max-w-2xl">
         Introducing our next generation platform for anonymous messages.{" "}
         <span className="text-white font-medium">v2.0</span> requires a new{" "}
         <Link href="/login" className="text-foreground underline font-medium">
@@ -39,7 +39,7 @@ export default async function Home() {
         that can be used across the platform.
       </p>
 
-      <div className="flex items-center gap-2 mt-8">
+      <div className="flex items-center gap-2">
         <Button size="lg" className="md:text-base" asChild>
           <Link href="/login">Continue</Link>
         </Button>
@@ -55,12 +55,6 @@ export default async function Home() {
       </div>
 
       <Demo />
-
-      <div className="mt-4 text-muted-foreground text-sm flex items-center">
-        <Lock className="h-4 w-4 mr-2" />
-        All messages are automatically encrypted
-        <Lock className="h-4 w-4 ml-2" />
-      </div>
     </main>
   );
 }
