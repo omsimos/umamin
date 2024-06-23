23580592c56238c84e9fe72250b95c8638580cb8
hyamero <daleban.dev@gmail.com>
2024-06-23 13:32:51 +0800

    chore: fix demo card border shine

--- packages/ui/src/components/ui/shine-border.tsx
+++ packages/ui/src/components/ui/shine-border.tsx
@@ -12,7 +12,7 @@ interface ShineBorderProps {
 }
 
 export default function ShineBorder({
-  borderRadius = 8,
+  borderRadius = 12,
   borderWidth = 1,
   duration = 14,
   color = "#fff",
@@ -39,7 +39,7 @@ export default function ShineBorder({
             "--background-radial-gradient": `radial-gradient(transparent,transparent, ${!(color instanceof Array) ? color : color.join(",")},transparent,transparent)`,
           } as React.CSSProperties
         }
-        className={`before:bg-shine-size before:absolute before:inset-[0] before:aspect-square before:h-full before:w-full before:rounded-[--border-radius] before:p-[--border-width] before:will-change-[background-position] before:content-[""] before:![-webkit-mask-composite:xor] before:[background-image:var(--background-radial-gradient)] before:[background-size:300%_300%] before:![mask-composite:exclude] before:[mask:var(--mask-linear-gradient)] motion-safe:before:animate-[shine-pulse_var(--shine-pulse-duration)_infinite_linear]`}
+        className={`before:bg-shine-size before:absolute before:inset-[0] before:aspect-square before:h-full before:w-full before:rounded-[--border-radius] before:p-[--border-width] before:will-change-[background-position] before:content-[""] before:![-webkit-mask-composite:xor] before:[background-image:var(--background-radial-gradient)] before:[background-size:300%_300%] before:![mask-composite:exclude] before:[mask:var(--mask-linear-gradient)] motion-safe:before:animate-[shine-pulse_var(--shine-pulse-duration)_infinite_linear] pointer-events-none opacity-20`}
       ></div>
       <div className={"h-full w-full rounded-[--border-radius-child]"}>
         {children}
