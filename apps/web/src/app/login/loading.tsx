788580843dba08b93ec81ad45cd8eca48a73dc5f
hyamero <daleban.dev@gmail.com>
2024-06-24 11:40:34 +0800

    chore: login skeleton loading ui

--- apps/web/src/app/login/loading.tsx
+++ apps/web/src/app/login/loading.tsx
@@ -2,9 +2,27 @@ import { Skeleton } from "@umamin/ui/components/skeleton";
 
 export default function Loading() {
   return (
-    <div className="container">
-      <Skeleton className="w-full max-w-lg h-[400px] rounded-lg mx-auto mt-36" />
-    </div>
+    <section className="max-w-lg md:max-w-md container mt-36 [&>div]:gap-3 [&>div]:flex [&>div]:flex-col flex gap-8 flex-col">
+      <div>
+        <Skeleton className="w-2/5 h-[25px] rounded-md" />
+        <Skeleton className="w-1/2 h-[10px] rounded-md" />
+      </div>
+
+      <div>
+        <Skeleton className="w-1/5 h-[10px] rounded-md" />
+        <Skeleton className="w-full h-[30px] rounded-md" />
+      </div>
+
+      <div>
+        <Skeleton className="w-1/5 h-[10px] rounded-md" />
+        <Skeleton className="w-full h-[30px] rounded-md" />
+      </div>
+
+      <div>
+        <Skeleton className="w-full h-[30px] rounded-md" />
+        <Skeleton className="w-full h-[30px] rounded-md" />
+        <Skeleton className="mx-auto w-1/2 h-[10px] rounded-md" />
+      </div>
+    </section>
   );
 }
-
