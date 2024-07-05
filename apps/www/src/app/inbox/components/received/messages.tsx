fe04622f5968406fc4711903ba9167fb989719c6
Josh Daniel <joshxfi.dev@gmail.com>
2024-07-05 10:46:22 +0800

    chore: move inbox tabs suspense to parent

--- apps/www/src/app/inbox/components/received/messages.tsx
+++ apps/www/src/app/inbox/components/received/messages.tsx
@@ -1,6 +1,4 @@
-import { cache, Suspense } from "react";
-import { Skeleton } from "@umamin/ui/components/skeleton";
-
+import { cache } from "react";
 import { getSession } from "@/lib/auth";
 import { getClient } from "@/lib/gql/rsc";
 import { ReceivedMessagesList } from "./list";
@@ -19,24 +17,14 @@ export async function ReceivedMessages() {
   const messages = await getMessages(session?.id);
 
   return (
-    <Suspense
-      fallback={
-        <div className="space-y-5">
-          <Skeleton className="w-full h-[200px] rounded-lg" />
-          <Skeleton className="w-full h-[200px] rounded-lg" />
-          <Skeleton className="w-full h-[200px] rounded-lg" />
-        </div>
-      }
-    >
-      <div className="flex flex-col items-center gap-5 pb-20">
-        {!messages?.length ? (
-          <p className="text-sm text-muted-foreground mt-4">
-            No messages to show
-          </p>
-        ) : (
-          <ReceivedMessagesList messages={messages} />
-        )}
-      </div>
-    </Suspense>
+    <div className="flex flex-col items-center gap-5 pb-20">
+      {!messages?.length ? (
+        <p className="text-sm text-muted-foreground mt-4">
+          No messages to show
+        </p>
+      ) : (
+        <ReceivedMessagesList messages={messages} />
+      )}
+    </div>
   );
 }
--- apps/www/src/app/inbox/components/sent/messages.tsx
+++ apps/www/src/app/inbox/components/sent/messages.tsx
@@ -1,6 +1,4 @@
-import { cache, Suspense } from "react";
-import { Skeleton } from "@umamin/ui/components/skeleton";
-
+import { cache } from "react";
 import { getSession } from "@/lib/auth";
 import { getClient } from "@/lib/gql/rsc";
 import { SentMessagesList } from "./list";
@@ -19,24 +17,14 @@ export async function SentMessages() {
   const messages = await getMessages(session?.id);
 
   return (
-    <Suspense
-      fallback={
-        <div className="space-y-5">
-          <Skeleton className="w-full h-[300px] rounded-lg" />
-          <Skeleton className="w-full h-[300px] rounded-lg" />
-          <Skeleton className="w-full h-[300px] rounded-lg" />
-        </div>
-      }
-    >
-      <div className="flex w-full flex-col items-center gap-5 pb-20">
-        {!messages?.length ? (
-          <p className="text-sm text-muted-foreground mt-4">
-            No messages to show
-          </p>
-        ) : (
-          <SentMessagesList messages={messages} />
-        )}
-      </div>
-    </Suspense>
+    <div className="flex w-full flex-col items-center gap-5 pb-20">
+      {!messages?.length ? (
+        <p className="text-sm text-muted-foreground mt-4">
+          No messages to show
+        </p>
+      ) : (
+        <SentMessagesList messages={messages} />
+      )}
+    </div>
   );
 }
