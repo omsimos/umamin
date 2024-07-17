8581a83b1015e662a47f09527c242a838921e76a
Josh Daniel <joshxfi.dev@gmail.com>
2024-07-17 19:30:55 +0800

    chore: wrap onShare on try catch

--- apps/www/src/app/components/share-button.tsx
+++ apps/www/src/app/components/share-button.tsx
@@ -5,22 +5,26 @@ import { analytics } from "@/lib/firebase";
 import { logEvent } from "firebase/analytics";
 
 const onShare = (username: string) => {
-  if (typeof window !== "undefined") {
-    const url = `${window.location.origin}/user/${username}`;
+  try {
+    if (typeof window !== "undefined") {
+      const url = `${window.location.origin}/user/${username}`;
 
-    if (
-      navigator.share &&
-      navigator.canShare({ url }) &&
-      process.env.NODE_ENV === "production"
-    ) {
-      navigator.share({ url });
-    } else {
-      navigator.clipboard.writeText(
-        `${window.location.origin}/user/${username}`,
-      );
+      if (
+        navigator.share &&
+        navigator.canShare({ url }) &&
+        process.env.NODE_ENV === "production"
+      ) {
+        navigator.share({ url });
+      } else {
+        navigator.clipboard.writeText(
+          `${window.location.origin}/user/${username}`
+        );
+      }
+
+      logEvent(analytics, "share_profile");
     }
-
-    logEvent(analytics, "share_profile");
+  } catch (err) {
+    console.log(err);
   }
 };
 
