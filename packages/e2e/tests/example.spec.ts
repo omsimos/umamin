8483e06f7e6991ef37bd1caa308e48701057ef19
Josh Daniel <joshxfi.dev@gmail.com>
2024-07-20 16:18:01 +0800

    chore: update: test placeholder

--- packages/e2e/tests/example.spec.ts
+++ packages/e2e/tests/example.spec.ts
@@ -1,18 +1,20 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
-test('has title', async ({ page }) => {
-  await page.goto('/');
+test("has title", async ({ page }) => {
+  await page.goto("/");
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  await expect(page).toHaveTitle(/Umamin — The Platform for Anonymity/);
 });
 
-test('get started link', async ({ page }) => {
-  await page.goto('/');
+test("umamin social link", async ({ page }) => {
+  await page.goto("/");
 
   // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+  await page.getByRole("link", { name: "Umamin Social" }).click();
 
   // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  await expect(
+    page.getByRole("heading", { name: "Umamin Social" })
+  ).toBeVisible();
 });
