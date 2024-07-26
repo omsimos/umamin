8218253a6057944987d443e946d0a6edc31f3602
Josh Daniel <joshxfi.dev@gmail.com>
2024-07-26 21:55:25 +0800

    chore(e2e): add script for playwright in ui mode

--- packages/e2e/tests/auth.spec.ts
+++ packages/e2e/tests/auth.spec.ts
@@ -14,35 +14,48 @@ test("should redirect unauthenticated users", async ({ page }) => {
   await expect(page).toHaveTitle(/Umamin — Login/);
 });
 
-test("ensure successful authentication flow", async ({ page }) => {
-  // create an account
-  await page.goto("/register");
-  await expect(page).toHaveTitle(/Umamin — Register/);
+test.describe.serial("Authentication", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto("/");
+  });
+  test("can register", async ({ page }) => {
+    await page.getByRole("link", { name: "Continue" }).click();
+    await expect(page).toHaveTitle(/Umamin — Register/);
 
-  await page.getByLabel("Username").fill(username);
-  await page.getByLabel("Password", { exact: true }).fill(password);
-  await page.getByLabel("Confirm Password").fill(password);
-  await page.getByRole("button", { name: "Create an account" }).click();
+    await page.getByLabel("Username").fill(username);
+    await page.getByLabel("Password", { exact: true }).fill(password);
+    await page.getByLabel("Confirm Password").fill(password);
+    await page.getByRole("button", { name: "Create an account" }).click();
 
-  await page.waitForURL("**/inbox");
-  await expect(page).toHaveTitle(/Umamin — Inbox/);
-  await expect(page.getByTestId("username")).toBeVisible();
+    await page.waitForURL("**/inbox");
+    await expect(page).toHaveTitle(/Umamin — Inbox/);
+    await expect(page.getByTestId("username")).toBeVisible();
+  });
 
-  // go to settings and log out
-  await page.getByTestId("nav-settings-btn").click();
-  await expect(page).toHaveTitle(/Umamin — Settings/);
-  await expect(page.getByRole("heading", { name: "Settings" })).toBeVisible();
+  test("can login", async ({ page }) => {
+    await page.getByTestId("nav-login-btn").click();
+    await page.waitForURL("**/login");
+    await expect(page).toHaveTitle(/Umamin — Login/);
 
-  await page.getByTestId("logout-btn").click();
-  await page.waitForURL("**/login");
-  await expect(page).toHaveTitle(/Umamin — Login/);
+    await page.getByLabel("Username").fill(username);
+    await page.getByLabel("Password").fill(password);
+    await page.getByRole("button", { name: "Login" }).click();
 
-  // login with the new account
-  await page.getByLabel("Username").fill(username);
-  await page.getByLabel("Password").fill(password);
-  await page.getByRole("button", { name: "Login" }).click();
+    await page.waitForURL("**/inbox");
+    await expect(page).toHaveTitle(/Umamin — Inbox/);
+    await expect(page.getByTestId("username")).toBeVisible();
 
-  await page.waitForURL("**/inbox");
-  await expect(page).toHaveTitle(/Umamin — Inbox/);
-  await expect(page.getByTestId("username")).toBeVisible();
+    // go to settings and logout
+    await page.getByTestId("nav-settings-btn").click();
+    await page.waitForURL("**/settings");
+    await expect(page).toHaveTitle(/Umamin — Settings/);
+    await expect(page.getByRole("heading", { name: "Settings" })).toBeVisible();
+
+    await page.getByTestId("logout-btn").click();
+    await page.waitForURL("**/login");
+    await expect(page).toHaveTitle(/Umamin — Login/);
+    await expect(
+      page.getByRole("heading", { name: "Umamin Account" })
+    ).toBeVisible();
+  });
 });
