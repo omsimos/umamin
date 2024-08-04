a28f734db4b56eb653c50e3a58652d0b98f17712
Josh Daniel <joshxfi.dev@gmail.com>
2024-08-04 16:29:55 +0800

    feat(social): add auth pages

--- apps/social/src/app/page.tsx
+++ apps/social/src/app/page.tsx
@@ -1,7 +1,20 @@
-export default function Home() {
+import { logout } from "@/actions";
+import { getSession } from "@/lib/auth";
+import { Button } from "@umamin/ui/components/button";
+
+export default async function Home() {
+  const { session } = await getSession();
+
   return (
     <main>
       <h1>Hello World</h1>
+      {session && (
+        <form action={logout}>
+          <Button type="submit" variant="outline">
+            Sign Out
+          </Button>
+        </form>
+      )}
     </main>
   );
 }
