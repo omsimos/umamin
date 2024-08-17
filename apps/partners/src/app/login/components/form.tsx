0d18e2a4726dd7a83df349df1e53fa0fb64cddf4
Josh Daniel <joshxfi.dev@gmail.com>
2024-08-17 16:13:32 +0800

    feat(partners): add login page

---
+++ apps/partners/src/app/login/components/form.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import { login } from "@/lib/actions";
+import { useFormState } from "react-dom";
+
+import { LoginButton } from "./login-button";
+import { Input } from "@umamin/ui/components/input";
+import { Label } from "@umamin/ui/components/label";
+
+export function LoginForm() {
+  const [state, formAction] = useFormState(login, { error: "" });
+
+  return (
+    <form action={formAction} className="space-y-6">
+      <div>
+        <Label htmlFor="username">Username</Label>
+        <Input
+          required
+          id="username"
+          name="username"
+          placeholder="umamin"
+          className="mt-2"
+        />
+      </div>
+
+      <div>
+        <Label htmlFor="password">Password</Label>
+        <Input
+          required
+          id="password"
+          name="password"
+          type="password"
+          className="mt-2"
+        />
+        {!!state?.error && (
+          <p className="text-red-500 text-sm mt-2 font-medium">{state.error}</p>
+        )}
+      </div>
+
+      <LoginButton />
+    </form>
+  );
+}
---
+++ apps/partners/src/app/login/page.tsx
@@ -0,0 +1,70 @@
+import Link from "next/link";
+import dynamic from "next/dynamic";
+import { getSession } from "@/lib/auth";
+import { redirect } from "next/navigation";
+import { LoginForm } from "./components/form";
+
+const BrowserWarning = dynamic(
+  () => import("@umamin/ui/components/browser-warning"),
+  { ssr: false }
+);
+
+export const metadata = {
+  title: "Umamin Partners — Login",
+  description:
+    "Log in to Umamin Partners to manage anonymous feedback, surveys, and communications securely. Enhance your workflow with powerful tools and maintain privacy at every step.",
+  keywords: [
+    "Umamin login",
+    "anonymous messaging login",
+    "encrypted messages login",
+  ],
+  robots: {
+    index: false,
+    follow: false,
+  },
+  openGraph: {
+    type: "website",
+    title: "Umamin Partners — Login",
+    description:
+      "Log in to Umamin Partners to manage anonymous feedback, surveys, and communications securely. Enhance your workflow with powerful tools and maintain privacy at every step.",
+    url: "https://partners.umamin.link/login",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Umamin Partners — Login",
+    description:
+      "Log in to Umamin Partners to manage anonymous feedback, surveys, and communications securely. Enhance your workflow with powerful tools and maintain privacy at every step.",
+  },
+};
+
+export default async function Login() {
+  const { user } = await getSession();
+
+  if (user) {
+    redirect("/inbox");
+  }
+
+  return (
+    <section className="max-w-lg md:max-w-md container mt-36 min-h-screen">
+      <BrowserWarning />
+
+      <div className="mb-6">
+        <h2 className="text-2xl tracking-tight font-semibold">
+          Umamin Account
+        </h2>
+        <p className="text-sm text-muted-foreground">
+          Proceed with your Umamin v2.0 profile
+        </p>
+      </div>
+
+      <LoginForm />
+
+      <div className="mt-4 text-center text-sm w-full">
+        Don&apos;t have an account?{" "}
+        <Link href="/register" className="underline">
+          Sign up
+        </Link>
+      </div>
+    </section>
+  );
+}
