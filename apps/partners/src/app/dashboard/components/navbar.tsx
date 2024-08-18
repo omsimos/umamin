009812257d38c8bef891452d3b757630ff329836
Josh Daniel <joshxfi.dev@gmail.com>
2024-08-18 18:32:30 +0800

    feat(partners): display messages

--- apps/partners/src/app/dashboard/components/navbar.tsx
+++ apps/partners/src/app/dashboard/components/navbar.tsx
@@ -4,14 +4,15 @@ import { SignOutButton } from "./sign-out-btn";
 
 export async function Navbar() {
   return (
-    <nav className="fixed left-0 right-0 top-0 z-50 w-full bg-background bg-opacity-40 bg-clip-padding py-5 backdrop-blur-xl backdrop-filter lg:z-40 container max-w-screen-xl flex justify-between items-center">
+    <nav className="fixed left-0 right-0 top-0 z-50 w-full bg-background bg-opacity-40 bg-clip-padding py-5 backdrop-blur-xl backdrop-filter lg:z-40 container max-w-screen-2xl flex justify-between items-center">
       <div className="space-x-2 flex items-center">
         <Link href="/" aria-label="logo">
+          <span className="text-muted-foreground font-medium">partners.</span>
           <span className="font-semibold text-foreground">umamin</span>
           <span className="text-muted-foreground font-medium">.link</span>
         </Link>
 
-        <Badge variant="outline">partners</Badge>
+        <Badge variant="outline">beta</Badge>
       </div>
 
       <SignOutButton />
--- apps/partners/src/app/dashboard/page.tsx
+++ apps/partners/src/app/dashboard/page.tsx
@@ -1,10 +1,18 @@
 import { getSession } from "@/lib/auth";
+import { redirect } from "next/navigation";
+import { ReceivedMessages } from "./components/received/messages";
 
 export default async function Dashboard() {
-  const { user } = await getSession();
+  const { user, session } = await getSession();
+
+  if (!session) {
+    redirect("/login");
+  }
+
   return (
-    <div className="max-w-screen-xl mx-auto mt-32 container">
+    <div className="max-w-screen-2xl mx-auto mt-32 container">
       <h1 className="text-4xl">Hello, {user?.displayName || user?.username}</h1>
+      <ReceivedMessages sessionId={session?.id} />
     </div>
   );
 }
