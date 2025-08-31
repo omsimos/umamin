51b784aa7a00354d0e3b6483cb69f2e5bf10192d
Josh Daniel Bañares <joshxfi.dev@gmail.com>
2025-08-31 18:29:40 +0800

    feat: setup mdx for privacy and terms

---
+++ apps/www/app/terms/page.tsx
@@ -0,0 +1,16 @@
+import { Metadata } from "next";
+import TermsOfService from "@/markdown/terms.mdx";
+
+export const metadata: Metadata = {
+  title: "Umamin — Terms of Service",
+  description:
+    "Understand the terms and conditions for using Umamin, an open-source platform for sending and receiving encrypted anonymous messages.",
+};
+
+export default function Page() {
+  return (
+    <div className="prose prose-zinc dark:prose-invert max-w-(--breakpoint-md) container min-h-screen lg:mt-24 mt-12 pb-24">
+      <TermsOfService />
+    </div>
+  );
+}
--- apps/www/next.config.ts
+++ apps/www/next.config.ts
@@ -1,7 +1,17 @@
+import createMDX from "@next/mdx";
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  compiler: {
+    removeConsole: process.env.NODE_ENV === "production",
+  },
 };
 
-export default nextConfig;
+const withMDX = createMDX({
+  options: {
+    remarkPlugins: ["remark-gfm"],
+  },
+});
+
+export default withMDX(nextConfig);
