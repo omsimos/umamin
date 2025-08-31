0787251f4dcedc8cf1d7d809557a4f15f30d686a
Josh Daniel Bañares <joshxfi.dev@gmail.com>
2025-08-31 23:50:13 +0800

    feat: setup ui package

--- packages/ui/src/components/dialog.tsx
+++ packages/ui/src/components/dialog.tsx
@@ -4,7 +4,7 @@ import * as React from "react"
 import * as DialogPrimitive from "@radix-ui/react-dialog"
 import { XIcon } from "lucide-react"
 
-import { cn } from "@/lib/utils"
+import { cn } from "@umamin/ui/lib/utils"
 
 function Dialog({
   ...props
--- packages/ui/src/components/dropdown-menu.tsx
+++ packages/ui/src/components/dropdown-menu.tsx
@@ -4,7 +4,7 @@ import * as React from "react"
 import * as DropdownMenuPrimitive from "@radix-ui/react-dropdown-menu"
 import { CheckIcon, ChevronRightIcon, CircleIcon } from "lucide-react"
 
-import { cn } from "@/lib/utils"
+import { cn } from "@umamin/ui/lib/utils"
 
 function DropdownMenu({
   ...props
