979f45da0f78f32b3b85da73429d30f42a787ea3
Josh Daniel Bañares <joshxfi.dev@gmail.com>
2025-09-11 21:48:54 +0800

    chore: add umamin notes header

--- apps/www/app/notes/page.tsx
+++ apps/www/app/notes/page.tsx
@@ -40,6 +40,10 @@ export default async function Page() {
 
   return (
     <div className="container max-w-xl space-y-12">
+      <h1 className="font-extrabold sm:text-5xl text-[9vw] leading-none dark:bg-gradient-to-b from-foreground dark:to-zinc-400 bg-zinc-800 bg-clip-text text-transparent tracking-tighter text-center my-6">
+        Umamin Notes
+      </h1>
+
       {session ? (
         <NoteForm />
       ) : (
