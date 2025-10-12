98bc77b5864af884c120c44dbbb7d8ecc7e3ddcb
Josh Daniel Bañares <joshxfi.dev@gmail.com>
2025-10-12 22:05:48 +0800

    chore: update quiet mode indicator

--- apps/www/app/notes/components/note-card.tsx
+++ apps/www/app/notes/components/note-card.tsx
@@ -7,7 +7,7 @@ import {
 import { Card, CardContent, CardHeader } from "@umamin/ui/components/card";
 import {
   BadgeCheckIcon,
-  MessageCircleOffIcon,
+  MessageSquareXIcon,
   MessageSquareTextIcon,
   ScanFaceIcon,
 } from "lucide-react";
@@ -100,7 +100,7 @@ export function NoteCard({ data, isAuthenticated }: Props) {
                 </span>
               )}
 
-              {user?.quietMode && <MessageCircleOffIcon className="size-4" />}
+              {user?.quietMode && <MessageSquareXIcon className="size-5" />}
 
               {isAuthenticated && !data.isAnonymous && !user?.quietMode && (
                 <button
