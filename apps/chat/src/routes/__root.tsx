d340f7e9b2dd9bfe8ef185d2ad49e86dddfb448d
Josh Daniel Bañares <joshxfi.dev@gmail.com>
2026-06-02 13:50:22 +0800

    feat: setup rotuer

---
+++ apps/chat/src/routes/__root.tsx
@@ -0,0 +1,15 @@
+import { createRootRoute, Outlet } from "@tanstack/react-router";
+import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
+
+export const Route = createRootRoute({
+  component: RootLayout,
+});
+
+function RootLayout() {
+  return (
+    <>
+      <Outlet />
+      <TanStackRouterDevtools />
+    </>
+  );
+}
